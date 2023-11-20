e9b5db9995e77b9057aaa35eb57477af3c9006ff
javier-river <javier.ramos5@misena.edu.co>
2023-11-20 13:13:25 -0500

    //importar la clase Router y crear una instancia del mismo

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {return view('welcome');});
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-    Route::get('/profile',[UsuarioController::class,'profile']);
-});
+// Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {Route::get('/dashboard', function ()
+// {return view('dashboard'); })->name('dashboard'); Route::get('/profile',[UsuarioController::class,'profile']);});
 
-Route::get('/auth/redirect',[AuthController::class,'redirect']);
-Route::get('/auth/callback-url',[AuthController::class,'redirect']);
+// Route::get('/auth/redirect',[AuthController::class,'redirect']);
+// Route::get('/auth/callback-url',[AuthController::class,'redirect']);
