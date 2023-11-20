3665ca5dae19185cfb0ac670078d316e154d0de0
javier-river <javier.ramos5@misena.edu.co>
2023-11-20 14:38:44 -0500

    //rutas

--- routes/api.php
+++ routes/api.php
@@ -3,18 +3,19 @@
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\CategoriaController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('/', function () {return view('welcome');});
+
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {Route::get('/dashboard', function ()
+{return view('dashboard'); })->name('dashboard'); Route::get('/profile',[UsuarioController::class,'profile']);});
+
+Route::get('/auth/redirect',[AuthController::class,'redirect']);
+Route::get('/auth/callback-url',[AuthController::class,'redirect']);
